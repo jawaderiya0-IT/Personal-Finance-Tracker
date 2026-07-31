5874fb8a9c3aa78b556cc54dd63b4032a93159a2
Riya jawade <jawaderiya0@gmail.com>
2026-07-31 20:34:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to Personal Finance Tracker")
+income = float(input("Enter your income: "))
+expense = float(input("Enter your expense: "))
+balance = income - expense
+print("Remaining Balance:",balance)
