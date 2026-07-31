442e9c2b3ad2c67a884f9e67eea1df43ff4da61c
Riya jawade <jawaderiya0@gmail.com>
2026-07-31 21:09:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-Balance = 0
+balance = 0
 while True:
   print("\n-----Personal Finance Tracker-----")
   print("1.Add Income")
   print("2.Add Expense")
-  print("3.Exit")
+  print("3.View Balance")
+  print("4.Exit")
   choice = input("Enter your choice: ")
   if choice == "1":
     income = float(input("Enter income:"))
@@ -14,7 +15,8 @@ while True:
     balance -= expense
     print("Current Balance:",balance)
  elif choice == "3":
-    print("Thank You!")
+    print("Current Balance:",balance)
+elif choice == "4":
+print("Thank You!")
     break
-  else:
-    print("Invalid Choice")
+ 
