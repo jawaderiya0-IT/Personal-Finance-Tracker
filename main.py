112a50a3857118d1c06cb09e3e5260b1bbfa4049
Riya jawade <jawaderiya0@gmail.com>
2026-07-31 20:57:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-print("Welcome to Personal Finance Tracker")
-income = float(input("Enter your income: "))
-expense = float(input("Enter your expense: "))
-balance = income - expense
-print("Remaining Balance:",balance)
+while True:
+  print("\n-----Personal Finance Tracker-----")
+  print("1.Add Income")
+  print("2.Add Expense")
+  print("3.Exit")
+  choice = input("Enter your choice: ")
+  if choice == "1":
+    income = float(input("Enter income:"))
+    print("Income Added:",Income)
+  elif choice == "2":
+    expense = float(input("Enter expense:")
+              print("Expense Added:",expense)
+ elif choice == "3":
+    print("Thank You!")
+    break
+  else:
+    print("Invalid Choice")
